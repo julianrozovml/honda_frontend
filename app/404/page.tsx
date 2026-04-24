6b85d94d7e571739c2f97147bfc918bd28a30329
Daniel Poveda <daniepovedagonza@gmail.com>
2026-04-24 10:31:03 -0500

    feat: create custom 404 page

---
+++ app/404/page.tsx
@@ -0,0 +1,39 @@
+import Container from "@/components/ui/Layout/Container/container";
+import Button from "@/components/ui/Buttons/Button/button";
+import { SliderOnlineStore } from "@/components/drupal/SliderOnlineStore/slider-online-store";
+import { MOCK_STORE_TABS } from "@/components/drupal/SliderOnlineStore/slider-online-store.mock";
+import Image from "next/image";
+import styles from "./404.module.scss";
+
+export default function Custom404Page() {
+  return (
+    <main>
+      <section className={styles.hero}>
+        <Container>
+          <div className={styles.heroInner}>
+            <p className={styles.label}>Error</p>
+            <h1 className={styles.code}>404</h1>
+
+            <Image
+              src="/images/robot-404.svg"
+              alt="Robot de error 404"
+              width={250}
+              height={250}
+              className={styles.illustration}
+              priority
+            />
+
+            <p className={styles.message}>
+              Lo sentimos, la pagina que estas buscando no existe o ha sido
+              movida.
+            </p>
+
+            <Button label="Volver al inicio" href="/" className={styles.cta} />
+          </div>
+        </Container>
+      </section>
+
+      <SliderOnlineStore tabs={MOCK_STORE_TABS} />
+    </main>
+  );
+}
