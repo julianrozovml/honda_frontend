5750c662e00ca58c6adbf8314c0051f4b85fd088
Daniel Poveda <daniepovedagonza@gmail.com>
2026-04-27 16:41:53 -0500

    feat(components): add product-highlights-text and update 404 mobile styles

--- app/404/page.tsx
+++ app/404/page.tsx
@@ -2,6 +2,7 @@ import Container from "@/components/ui/Layout/Container/container";
 import Button from "@/components/ui/Buttons/Button/button";
 import { SliderOnlineStore } from "@/components/drupal/SliderOnlineStore/slider-online-store";
 import { MOCK_STORE_TABS } from "@/components/drupal/SliderOnlineStore/slider-online-store.mock";
+import ProductHighlightsText from "@/components/drupal/ProductHighlightsText/product-highlights-text";
 import Image from "next/image";
 import styles from "./404.module.scss";
 
@@ -32,6 +33,10 @@ export default function Custom404Page() {
           </div>
         </Container>
       </section>
+
+      <Container className={styles.highlightPreview}>
+        <ProductHighlightsText text="La nueva scooter PCX 160 es MÁS QUE UNA Motocicleta MAS, su Comfort y Tecnologías te sorprenderán para vivir una experiencia diferente a otra scoorter.​  QUE SIGNIFICA PCX:​ Personal Comfort Experience o una Experiencia de Comodidad Personalizada para sus usuarios.​" />
+      </Container>
 
       <SliderOnlineStore tabs={MOCK_STORE_TABS} />
     </main>
